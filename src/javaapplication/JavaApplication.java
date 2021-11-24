0630338b457651cd1f15c780d23589a80d3570d7
apiwitwutithumkitti <apiwit.wutithumkitti@outlook.com>
2021-11-24 22:44:07 +0700

    add method Add()

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -3,8 +3,12 @@ package javaapplication;
 public class JavaApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	
+	private int Add(int number1, int number2) {
+		int sum = number1 + number2;
+		return sum;
 	}
 
 }
