1687bdfc7ca1a948c940141c14ff54dc065d8c9d
apiwitwutithumkitti <apiwit.wutithumkitti@outlook.com>
2021-11-24 22:50:37 +0700

    add Feature-C

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -3,8 +3,7 @@ package javaapplication;
 public class JavaApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Feature C");
 	}
-
+	
 }
